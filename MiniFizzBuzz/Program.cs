7378d441ed2047ece20d08189c6e2b04c47698bd
kevingilfether <kevingilfether@gmail.com>
2017-09-28 09:30:23 -0400

    Mini Fizz Buzz w. if

---
+++ MiniFizzBuzz/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MiniFizzBuzz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // This is MiniFizzBuzz
+            Console.WriteLine("Give me a number:");
+            int fizz = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("And another:");
+            int buzz = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("I'm hungry, give me a test number.");
+            int test = int.Parse(Console.ReadLine());
+
+            //Conditionals taking test number and returning whether multiple of them:
+            if (test % fizz == 0 && test % buzz == 0)
+            {
+                Console.WriteLine("FizzBuzz!");
+            }
+            else if (test % fizz == 0)
+            {
+                Console.WriteLine("Fizz!");
+            }
+            else if (test % buzz == 0)
+            {
+                Console.WriteLine("Buzz!");
+            }
+            else
+            {
+                Console.WriteLine(test);
+            }
+        }
+    }
+}
